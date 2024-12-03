a8fe2bdf32ecc132d0f9b9ade97db9b19d7ca5d9
mdsamaul <mdsamaul843@gmail.com>
2024-12-03 21:19:24 +0600

    again push

--- Oline-Ride-Share-idb-project.Server/obj/Debug/net8.0/Oline-Ride-Share-idb-project.Server.AssemblyInfo.cs
+++ Oline-Ride-Share-idb-project.Server/obj/Debug/net8.0/Oline-Ride-Share-idb-project.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Oline-Ride-Share-idb-project.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d3699cfaccb9c45301b0dc615c2154607609ce5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eda987968005b38cbf84c0675d206acffd410a7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Oline-Ride-Share-idb-project.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Oline-Ride-Share-idb-project.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
