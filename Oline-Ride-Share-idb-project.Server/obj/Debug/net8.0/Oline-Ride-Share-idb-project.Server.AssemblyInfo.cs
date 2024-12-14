f580e565629f570bf5d348b6c4af324e8211c479
mdsamaul <mdsamaul843@gmail.com>
2024-12-14 23:49:38 +0600

    chgange

--- Oline-Ride-Share-idb-project.Server/obj/Debug/net8.0/Oline-Ride-Share-idb-project.Server.AssemblyInfo.cs
+++ Oline-Ride-Share-idb-project.Server/obj/Debug/net8.0/Oline-Ride-Share-idb-project.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Oline-Ride-Share-idb-project.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f39cbc04a6ca1c556422175c84ac445a62ff834")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78390447dfe8073bc682a06d36dc0847bbc797e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Oline-Ride-Share-idb-project.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Oline-Ride-Share-idb-project.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
